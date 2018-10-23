d657474c44108150516308970c48722bbdf50170
Mikaner <c0118259dc@edu.teu.ac.jp>
2018-10-23 23:10:13 +0900

    入力の続き。何も入力しない時にwhile文を出たいのに出れないのなんでなん...まあ、大体入力はこんな感じで

--- Enigma.java
+++ Enigma.java
@@ -1,12 +1,43 @@
 import java.util.Scanner;
 public class Enigma{
     public static void main(String[] args) {
-        //文字列の入力
+        //input
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Setting");
-        //int[] roterNum = scanner.
-        int sNum = scanner.nextInt();
+        System.out.println("First Roter");
+        int sNum1 = scanner.nextInt();//the Number of First roter
+        char top1 = scanner.next().charAt(0); //first character
+
+        System.out.println("Second Roter");
+        int sNum2 = scanner.nextInt();//the Second roter
+        char top2 = scanner.next().charAt(0);//first
+
+        System.out.println("Third Roter");
+        int sNum3 = scanner.nextInt();//the Third
+        char top3 = scanner.next().charAt(0);//first
+
+        System.out.println("Exchange");
+        char char1 = scanner.next().charAt(0);//exchange character
+        char char2 = scanner.next().charAt(0);
+
+        Roter roter1 = new Roter(sNum1,top1);//call roter1
+        Roter roter2 = new Roter(sNum2,top2);//call roter2
+        Roter roter3 = new Roter(sNum3,top3);//call roter3
+        Roter.setExchange(char1, char2);
+        System.out.println(Roter.getRoterNum());
+
         System.out.println("Please type a message here.");
-        String input = scanner.next();
+        while(true){
+            String input = scanner.next();
+            /*if(input == ""){ I don't know why this don't move.
+                break;
+            }*/
+            char[] cPut = input.toCharArray();//change type of char
+            for(int i = 0; i < cPut.length; i++){
+                System.out.println(cPut[i]);
+            }
+        }
+
     }
 }
--- Roter.java
+++ Roter.java
@@ -1,11 +1,30 @@
 public class Roter{
     private int num;
+    private char top;
+    private static char char1,char2;
+    private static int roterNum;
     
-    public Roter(int num){
+    public Roter(int num, char top){
         this.num = num;
+        this.top = top;
+        roterNum++;
     }
 
-    public setupRoter(cher c){
+    public static void setExchange(char c1, char c2){
+        char1 = c1;
+        char2 = c2;
+    }
 
+    public int getNum(){
+        return num;
+    }
+    public char getTop(){
+        return top;
+    }
+    public String getExchange(){
+        return char1+" "+char2;
+    }
+    public static int getRoterNum(){
+        return roterNum;
     }
 }
