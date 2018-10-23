2a42d5608f6beae32dc6376d27b356102f550430
Mikaner <c0118259dc@edu.teu.ac.jp>
2018-10-23 21:16:36 +0900

    さっきミスった

--- Enigma.java
+++ Enigma.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
 public class Enigma{
     public static void main(String[] args) {
-        Roter 
+        //文字列の入力
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Setting");
+        //int[] roterNum = scanner.
+        int sNum = scanner.nextInt();
+        System.out.println("Please type a message here.");
+        String input = scanner.next();
     }
 }
