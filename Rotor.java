0ea11daf562ec56c327e89c67845dc826fb39bdf
Mikaner <c0118259dc@edu.teu.ac.jp>
2018-10-31 16:58:35 +0900

    reRotor.pyの処理をRotor.javaの中に変更、いらないファイルを削除

--- Rotor.java
+++ Rotor.java
@@ -15,16 +15,16 @@ public class Rotor{
     private static int[] exchangeRotor = new int[alphSum];
                                    //{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25}
     private static int[] rotor1 =    { 6, 9,18,25,13, 7,24,21,19,16,20,23, 8, 3,12, 1,22,10,14,15, 2, 0, 4,17,11, 5};
-    private static int[] reRotor1 =  {21,15,20,13,22,25, 0, 5,12, 1,17,24,14, 4,18,19, 9,23, 2, 8,10, 7,16,11, 6, 3};
     private static int[] rotor2 =    { 3, 1,10,13, 6,22, 5,18,17,14,23, 9, 7,24,21,15, 8,16,12,19,25, 4,20, 0, 2,11};
-    private static int[] reRotor2 =  {23, 1,24, 0,21, 6, 4,12,16,11, 2,25,18, 3, 9,15,17, 8, 7,19,22,14, 5,10,13,20};
     private static int[] rotor3 =    {20, 9, 4,13,19, 5, 8,15,18,21,12, 0,11, 6,22, 7,23,24,10, 3,25,14,16, 1,17, 2};
-    private static int[] reRotor3 =  {11,23,25,19, 2, 5,13,15, 6, 1,18,12,10, 3,21, 7,22,24, 8, 4, 0, 9,14,16,17,20};
     private static int[] rotor4 =    {15, 8, 5,22, 2,17,16,21,10,11,18, 1,13,23,24,12,14, 6, 7, 0,19, 4, 3, 9,25,20};
-    private static int[] reRotor4 =  {19,11, 4,22,21, 2,17,18, 1,23, 8, 9,15,12,16, 0, 6, 5,10,20,25, 7, 3,13,14,24};
     private static int[] rotor5 =    { 1,16,23,18,15,20,22, 4, 7,21, 6,17, 3, 9,25, 2,24, 8,14,13,12, 5,11, 0,19,10};
-    private static int[] reRotor5 =  {23, 0,15,12, 7,21,10, 8,17,13,25,22,20,19,18, 4, 1,11, 3,24, 5, 9, 6, 2,16,14};
     private static int[] reflector = {25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    private static int[] reRotor1 =  new int[alphSum];//{21,15,20,13,22,25, 0, 5,12, 1,17,24,14, 4,18,19, 9,23, 2, 8,10, 7,16,11, 6, 3};
+    private static int[] reRotor2 =  new int[alphSum];//{23, 1,24, 0,21, 6, 4,12,16,11, 2,25,18, 3, 9,15,17, 8, 7,19,22,14, 5,10,13,20};
+    private static int[] reRotor3 =  new int[alphSum];//{11,23,25,19, 2, 5,13,15, 6, 1,18,12,10, 3,21, 7,22,24, 8, 4, 0, 9,14,16,17,20};
+    private static int[] reRotor4 =  new int[alphSum];//{19,11, 4,22,21, 2,17,18, 1,23, 8, 9,15,12,16, 0, 6, 5,10,20,25, 7, 3,13,14,24};
+    private static int[] reRotor5 =  new int[alphSum];//{23, 0,15,12, 7,21,10, 8,17,13,25,22,20,19,18, 4, 1,11, 3,24, 5, 9, 6, 2,16,14};
     private static char[] alphabet = new char[alphSum];
     private static char[] alphabetExchange = new char[alphSum];
     private static final Map<Character, Integer> alphabetToNum;
@@ -35,6 +35,11 @@ public class Rotor{
             alphabet[i] = (char)(firstAlph+i);
             exchangeRotor[i] = i;
             alphabetExchange[i] = alphabet[i];
+            reRotor1[rotor1[i]] = i;
+            reRotor2[rotor2[i]] = i;
+            reRotor3[rotor3[i]] = i;
+            reRotor4[rotor4[i]] = i;
+            reRotor5[rotor5[i]] = i;
         }
         alphabetToNum = Collections.unmodifiableMap(map);
     }
@@ -72,7 +77,7 @@ public class Rotor{
                 reRotor[i] = (alphSum+reRotor5[(top+i)%alphSum]-top)%alphSum;
             }
         }else{
-            System.out.println("Please set the number of rotor between 1 and 4.");
+            System.out.println("Please set the number of rotor between 1 and 5.");
         }
     }
 
--- reRotor.py
+++
@@ -1,7 +0,0 @@
-s = list(map(int,input().split(",")))
-out = [0]*len(s)
-
-for i in range(len(s)):
-    out[s[i]] = i
-
-print(out)
